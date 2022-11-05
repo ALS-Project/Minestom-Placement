7e33d795469fcf854f19b40b661a9f247f907aaa
MrBretzel <axelolo68@gmail.com>
2022-11-05 19:19:06 +0100

    Added a comment for all PlacementRule

--- src/main/java/fr/bretzel/minestom/placement/PlacementRule.java
+++ src/main/java/fr/bretzel/minestom/placement/PlacementRule.java
@@ -18,11 +18,37 @@ public abstract class PlacementRule {
         return block;
     }
 
+    /**
+     * @param instance      the instance when the block is trying to be placed
+     * @param blockFace     the right-clicked face for the player
+     * @param blockPosition the block position of the block
+     * @param blockState    a blocksate integration
+     * @param pl            the player
+     * @return true if the block can be placed
+     */
     public abstract boolean canPlace(Instance instance, Facing blockFace, Point blockPosition, BlockState blockState, Player pl);
 
+    /**
+     * @param instance      the instance when the block is trying to be placed
+     * @param blockPosition the block position of the block
+     * @param blockState    a blocksate integration
+     * @return true if the block can be updated
+     */
     public abstract boolean canUpdate(Instance instance, Point blockPosition, BlockState blockState);
 
+    /**
+     * @param instance      the instance when the block is trying to be placed
+     * @param blockPosition the block position of the block
+     * @param blockState    a blocksate integration
+     */
     public abstract void update(Instance instance, Point blockPosition, BlockState blockState);
 
+    /**
+     * @param instance      the instance when the block is trying to be placed
+     * @param blockFace     the right-clicked face for the player
+     * @param blockPosition the block position of the block
+     * @param blockState    a blocksate integration
+     * @param pl            the player
+     */
     public abstract void place(Instance instance, BlockState blockState, Facing blockFace, Point blockPosition, Player pl);
 }
